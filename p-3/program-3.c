b16c6a30d073d9a54c86858cb8ad660f61a9a7a3
NAZMUS SAKIB <sakibabir52@gmail.com>
2022-04-11 20:50:26 +0600

    hello world

---
+++ p-3/program-3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int price = 56;
+    printf("Price = %d", price);
+
+    float temp = 29.50;
+    printf("temp = %f", temp);
+
+    doble tempTow = 69.69;
+    printf("tempTow = %lf", tempTow);
+}
