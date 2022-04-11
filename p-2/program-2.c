5ba57621ed8f5bf3d8e5294cdea3e1fcf91700fd
NAZMUS SAKIB <sakibabir52@gmail.com>
2022-04-11 18:56:12 +0600

    exploring c

---
+++ p-2/program-2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+	int marks = 10;
+	marks = marks + 10;
+	printf("%d ", marks);
+
+	marks = marks - 50;
+	marks = 20 + 5;
+	marks = marks * 2;
+	printf("%d ", marks);
+	return 0;
+}
