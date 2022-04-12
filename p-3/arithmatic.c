5e9b8d9e60f94c0584665ab4ebbca01688b471f2
NAZMUS SAKIB <sakibabir52@gmail.com>
2022-04-12 21:35:20 +0600

    arithmatic operator

--- p-3/arithmatic.c
+++ p-3/arithmatic.c
@@ -6,8 +6,16 @@ int main() {
     scanf("%d %d", &num1, &num2);
 
     int sum = num1 + num2;
-
     printf("%d + %d = %d\n", num1, num2, sum);
+
+    int biyog = num1 - num2;
+    printf("%d - %d = %d\n", num1, num2, biyog);
+
+    int multiplications = num1 * num2;
+    printf("%d * %d = %d\n", num1, num2, multiplications);
+
+    int devide = num1 / num2;
+    printf("%d / %d = %d\n", num1, num2, devide);
     
     return 0;
 }
