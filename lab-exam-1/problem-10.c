6b03b11fa903989127a932ecb1273a720459e683
NAZMUS SAKIB <nazmus.sakibbb1u@gmail.com>
2022-04-30 21:59:30 +0600

    problem 10 solved

---
+++ lab-exam-1/problem-10.c
@@ -0,0 +1,18 @@
+// Problem 10
+
+#include<stdio.h>
+
+int main() {
+    int x;
+
+    scanf("%d", &x);
+
+    if (x < 1200) {
+        printf("ABC");
+    }
+    else {
+        printf("ARC");
+    }
+
+    return 0;
+}
